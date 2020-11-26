b1511c24bf9015f8a2a934227da201583f663566
Sam Sanoop <sanoop_sam@hotmail.com>
2020-11-26 18:51:08 +0000

    Create server.go

---
+++ Scripts and pocs/server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8a\\r\\nSet-cookie: injected=value")
+	w.Write([]byte(`{"hello": "world"}`))
+}
+
+func main() {
+	http.HandleFunc("/", helloWorld)
+	http.ListenAndServe(":8000", nil)
+}
