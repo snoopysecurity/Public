35a23689f0d643ddf4aed8ff89c7b2c697bf5bd7
Sam Sanoop <sanoop_sam@hotmail.com>
2021-04-30 22:36:43 +0100

    Create main.rs

---
+++ Scripts and pocs/main.rs
@@ -0,0 +1,24 @@
+extern crate tar;
+
+use std::io::prelude::*;
+use std::fs::File;
+use tar::Archive;
+
+fn main() {
+    let file = File::open("/home/snoopy/evilarc/evil.tar").unwrap();
+    let mut a = Archive::new(file);
+
+    for file in a.entries().unwrap() {
+        // Make sure there wasn't an I/O error
+        let mut file = file.unwrap();
+
+        // Inspect metadata about the file
+        println!("{:?}", file.header().path().unwrap());
+        println!("{}", file.header().size().unwrap());
+
+        // files implement the Read trait
+        let mut s = String::new();
+        file.read_to_string(&mut s).unwrap();
+        println!("{}", s);
+    }
+}
